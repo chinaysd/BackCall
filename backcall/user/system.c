fc3cb58020dc15533d944af365bcc236b8539356
ysd <ysd@emomo.com>
2018-09-17 15:54:51 +0800

    [dev_ysd]代码优化

--- backcall/user/system.c
+++ backcall/user/system.c
@@ -6,58 +6,28 @@ void System_Init(void)
 	Led_Init();
 }
 
+
 void Delay_Time(unsigned int timer)
 {
 	unsigned int i,j;
-	for(i = timer;i > 0 ; i--)
+	for(i = timer; i > 0; i --)
 	{
-		for(j = 200; j > 0 ; j --);
+		for(j =200; j > 0; j--);
 	}
 }
 
-
-int PrintNum_Open(int n){  
-   //printf("Test1 is called,the number is %d\n",n); 
-   Led_Open();
-	n ++;
-	return 1;
-}
-
-int PrintNum_Close(int n){  
-   //printf("Test1 is called,the number is %d\n",n); 
-   Led_Close();
-	n ++;
-	return 1;
-}
-  
-void ShowNum(int n,void (* ptr)(int)){  
-   (* ptr)(n);  
+void BackCall(unsigned int timer,void (* ptr)(unsigned int))
+{
+	(* ptr)(timer);
 }
 
 
 void System_Handle(void)
 {
-	ShowNum(1000,PrintNum_Open);
-	Delay_Time(5000);
-	ShowNum(1000,PrintNum_Close);
-	Delay_Time(5000);
+	Led_Open();
+	BackCall(5000,Delay_Time);
+	Led_Close();
+	BackCall(5000,Delay_Time);
 }
-
-#if 0
-void BackCall(void (* ptr)())
-{
-	(* ptr)();
-}
-
-
-void BackCall(void (* ptr)())
-{
-	
-}
-#endif
-
-
-
-
 
 
